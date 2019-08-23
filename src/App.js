c2b23b016ea15509914f029318382e93ce21b8eb
Njabulo Mbuli <admin@njabulombuli.co.za>
2019-08-23 20:43:14 +0200

    uploading files

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import Navigation from './components/navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import 'tachyons';
+import './App.css';
+
+
+const particlesOptions={
+                particles: {
+                  number:{
+                    value:50,
+                    density:{
+                      enable:true,
+                      value_area:400
+                    }
+                  }
+                  }
+                }
+              
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      input:'',
+    }
+  }
+
+  onButtonSubmit=()=>{
+    console.log("click");
+    app.models.predict
+  }
+
+  onInputChange=(event)=>{
+    console.log(event.target.value);
+  }
+
+render(){
+  return(
+    <div className="App">
+      <Particles className="particles"
+              params={particlesOptions}/>
+      <Navigation/>
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm onInputChange={this.onInputChange}
+      onButtonSubmit={this.onButtonSubmit}/>
+    
+     {/*<FaceRecognition/ >*/}
+    </div>
+  );
+}
+}
+export default App;
---
+++ src/Leff.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+ const Leff = () => {
+ 	return(
+		<div class="stage">
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		  <div class="layer"></div>
+		</div>
+ 		);
+}
+
+export default Leff;
